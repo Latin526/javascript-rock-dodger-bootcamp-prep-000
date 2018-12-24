e714ba76acb7dd4a834600a80c778f77fa37f263
Yasmin Torres <yasmintorres90@hotmail.com>
2018-12-24 13:08:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -103,7 +103,7 @@ function moveDodgerRight() {
     if (positionToInteger(DODGER.style.left) < GAME_WIDTH - 40) {
       DODGER.style.left = `${positionToInteger(DODGER.style.left) + 1}px`;
     } else {
-      return
+      return;
     }
 
     move-- > 0 ? window.requestAnimationFrame(stepRight) : false;
@@ -113,15 +113,15 @@ function moveDodgerRight() {
 }
 
 function positionToInteger(p) {
-  return parseInt(p.split('px')[0]) || 0
+  return parseInt(p.split('px')[0]) || 0;
 }
 
 function start() {
-  window.addEventListener('keydown', moveDodger)
+  window.addEventListener('keydown', moveDodger);
 
-  START.style.display = 'none'
+  START.style.display = 'none';
 
   gameInterval = setInterval(function() {
-    createRock(Math.floor(Math.random() *  (GAME_WIDTH - 20)))
-  }, 1000)
+    createRock(Math.floor(Math.random() *  (GAME_WIDTH - 20)));
+  }, 1000);
 }
